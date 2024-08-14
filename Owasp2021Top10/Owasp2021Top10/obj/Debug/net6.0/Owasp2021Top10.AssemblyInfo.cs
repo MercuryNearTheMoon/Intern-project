73bd730822fa5548a5aca86fd805b1dcb16e5d1d
Mercury HUAN(黃信誌) <mercuryhuang@gamania.com>
2024-08-14 14:11:33 +0800

    Add insecrity role to auth restock function

--- Owasp2021Top10/Owasp2021Top10/obj/Debug/net6.0/Owasp2021Top10.AssemblyInfo.cs
+++ Owasp2021Top10/Owasp2021Top10/obj/Debug/net6.0/Owasp2021Top10.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Owasp2021Top10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d30a9bdcda189c4ad8c0d8556fd78ade3f23ac59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9afe8a048a8aa9c2c353dd48860696fbf2172d21")]
 [assembly: System.Reflection.AssemblyProductAttribute("Owasp2021Top10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Owasp2021Top10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
